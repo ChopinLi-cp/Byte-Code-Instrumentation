50882bfef25eb95e8a307a0c2275eeb009e464ca
Shanto Rahman <bit0321@iit.du.ac.bd>
2021-11-16 23:02:55 -0600

    Delete hard-coded  directory name(other)

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -5,8 +5,8 @@ currentDir0="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $currentDir0"/../agent"
 mvn install -DskipTests
 
-cd $currentDir0"/agent-pom-modify"
-bash modify-project.sh $currentDir0"/../other"
+#cd $currentDir0"/agent-pom-modify"
+#bash modify-project.sh $currentDir0"/../other"
 
 cd $currentDir0"/../other"
 mvn test
