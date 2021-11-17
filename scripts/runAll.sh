dcfe636139d56ee1898df9571785b4b94c524f0d
shanto-admin <bit0321@iit.du.ac.bd>
2021-11-17 12:35:59 -0600

    Change Run Scripts

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -9,15 +9,13 @@ input1=$1
 
 currentDir0="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+echo $currentDir0
+
 cd $currentDir0"/../agent"
 mvn install -DskipTests
 
-#cd $currentDir0"/agent-pom-modify"
-#bash modify-project.sh $currentDir0"/projects/$input1"
+cd $currentDir0"/../$input1"
+echo pwd
+mvn install # -DskipTests -Ddependency-check.skip=true -Denforcer.skip=true -Drat.skip=true -Dmdep.analyze.skip=true -Dmaven.javadoc.skip=true -Dgpg.skip -Dlicense.skip=true -Dskip=true
 
-cd $currentDir0"/projects/$input1"
-mvn install -DskipTests -Ddependency-check.skip=true -Denforcer.skip=true -Drat.skip=true -Dmdep.analyze.skip=true -Dmaven.javadoc.skip=true -Dgpg.skip -Dlicense.skip=true -Dskip=true
-
-bash $currentDir0/agent-pom-modify/modify-project.sh $currentDir0"/projects/$input1"
-
-mvn test -Ddependency-check.skip=true -Denforcer.skip=true -Drat.skip=true -Dmdep.analyze.skip=true -Dmaven.javadoc.skip=true -Dgpg.skip -Dlicense.skip=true -Dskip=true
+mvn test # -Ddependency-check.skip=true -Denforcer.skip=true -Drat.skip=true -Dmdep.analyze.skip=true -Dmaven.javadoc.skip=true -Dgpg.skip -Dlicense.skip=true -Dskip=true
