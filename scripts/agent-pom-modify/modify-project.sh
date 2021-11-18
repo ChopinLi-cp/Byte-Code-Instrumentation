01417040ca3230373c8db9e06c914ed67746cb2e
Shanto Rahman <bit0321@iit.du.ac.bd>
2021-11-18 15:57:19 +0000

    Checking Argument is given

--- scripts/agent-pom-modify/modify-project.sh
+++ scripts/agent-pom-modify/modify-project.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+if [[ $1 == "" ]]; then
+	echo "arg1 - the slug of the project"
+	exit
+fi
+
+project_path=$1
+
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 THE_PATH_TO_AGENT_JAR=$currentDir"/../../agent/target"
 ARG_LINE="-javaagent:$THE_PATH_TO_AGENT_JAR/agent-0.1-SNAPSHOT.jar"
 
 crnt=`pwd`
 working_dir=`dirname $0`
-project_path=$1
+#project_path=$1
 
 cd ${project_path}
 project_path=`pwd`
